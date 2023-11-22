4bbc37366364ecfe863e2e179184c61e2a932155
Valentina Valverde <vvgreenville@gmail.com>
2023-11-22 15:34:42 -0500

    Successfully got Tailwind to work.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <p>Hello world!</p>
-      <p className="text-3xl font-bold underline">Hello world!</p>
+      <p className="text-3xl font-bold">Hello world!</p>
     </>
   );
 }
