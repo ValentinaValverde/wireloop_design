97452e3bae3b22f63a9d59e57c1112c03d12f0a7
Valentina Valverde <vvgreenville@gmail.com>
2023-11-23 00:22:49 -0500

    Messed around a little bit with tailwind and added a header with a drop-down menu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,90 @@
 function App() {
   return (
     <>
-      <p>Hello world!</p>
-      <p className="text-3xl font-bold">Hello world!</p>
+      <header>
+        <nav className="nav">
+          <div>
+            <p className="business-name">wireloop</p>
+          </div>
+          <div className="relative inline-block text-left">
+            <div>
+              <button
+                type="button"
+                // className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+              >
+                <img
+                  className="inline-block h-10 w-10 rounded-full ring- ring-white"
+                  src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+                  alt=""
+                />
+              </button>
+            </div>
+            <div className="py-1" role="none">
+              {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+
+              <div
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex="-1"
+              >
+                <div className="py-1" role="none">
+                  {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+
+                  <div className="center-img">
+                    <img
+                      className="inline-block h-20 w-20 rounded-full ring- ring-white"
+                      src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+
+                  <p className="text-center text-sm">User Name</p>
+                  <a
+                    href="#"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-0"
+                  >
+                    Profile
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-1"
+                  >
+                    Groups
+                  </a>
+                </div>
+                <div className="py-1" role="none">
+                  <a
+                    href="#"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-0"
+                  >
+                    Log Out
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {/* spash page starts here */}
+      <div className="splash-page-container">
+        <p>welcome to</p>
+        <p className="h1">wireloop</p>
+      </div>
+      <div></div>
     </>
   );
 }
