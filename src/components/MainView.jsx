ed2a222313dde349f963c520cc19f837de98cc85
Valentina Valverde <vvgreenville@gmail.com>
2023-11-27 19:36:03 -0500

    Deleted cards and sideComponents files.

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,13 +1,37 @@
-import Card from "./Cards";
-import SideComponent from "./SideComponents";
-
 export default function MainView() {
   return (
     <>
       <div className="flex justify-between ">
-        <SideComponent />
-        <Card />
+        <p>something goes here... </p>
       </div>
+    </>
+  );
+}
+
+export function SingleCard() {
+  return (
+    <>
+      <div className="w-auto h-auto m-5 p-5 flex flex-col bg-white">
+        <img
+          className="rounded-md h-80"
+          src="https://images.pexels.com/photos/59884/pexels-photo-59884.jpeg"
+        />
+        <p>person</p>
+        <p>info / bio</p>
+        <p>interest tags</p>
+      </div>
+    </>
+  );
+}
+
+// this is the checkbox component that can be used.
+export function Checkbox() {
+  return (
+    <>
+      <label>
+        <input type="checkbox" id="VkvxHsg4bDQunS7b_Laravel" /> item
+      </label>
+      <br />
     </>
   );
 }
